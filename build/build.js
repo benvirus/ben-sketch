6666095333604f710b35b210a3294553884ad937
chenhaijiao <chenhaijiao@howzhi.com>
2017-08-07 16:28:38 +0800

    add dist file to git ignore and add the dist file to the npm package

---
+++ build/build.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack');
+const webpackConf = require('./webpack-config/base.js');
+
+const compile = webpack(webpackConf);
+compile.run(function() {
+    console.log('Finished Build!')
+});
