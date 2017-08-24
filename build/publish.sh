da3d7875c2ac3bb23ef173b5c7cc22497516d866
chenhaijiao <chenhaijiao@howzhi.com>
2017-08-24 10:52:06 +0800

    编写发布脚本

---
+++ build/publish.sh
@@ -0,0 +1,16 @@
+echo '> npm run build...'
+npm run build
+
+echo '> npm version patch...'
+version=$(npm version patch)
+
+echo '> npm publish...'
+npm publish
+
+echo '> push gitlab...'
+git push origin
+git push origin $version
+
+echo '> push github...'
+git push github
+git push github $version
