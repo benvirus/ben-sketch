4ee748d09ab967e269e2caa1b371a100fe4d2dd1
chenhaijiao <chenhaijiao@howzhi.com>
2017-08-24 11:05:40 +0800

    fix

--- build/publish.sh
+++ build/publish.sh
@@ -2,15 +2,15 @@ echo '> npm run build...'
 npm run build
 
 echo '> npm version patch...'
-version=$(npm version patch)
+version=$(npm version $1)
 
-echo '> npm publish...'
-npm publish
+# echo '> npm publish...'
+# npm publish
 
-echo '> push gitlab...'
-git push origin
-git push origin $version
+# echo '> push gitlab...'
+# git push origin
+# git push origin $version
 
-echo '> push github...'
-git push github
-git push github $version
+# echo '> push github...'
+# git push github
+# git push github $version
